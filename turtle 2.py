74ddbcef49165c11df80ded696e377b575c5f8b0
Hayder K. Alhamadi <50412844+haidarfmg@users.noreply.github.com>
2022-04-20 12:04:49 +0300

    draw star

---
+++ turtle 2.py
@@ -0,0 +1,7 @@
+# import turtle library
+import turtle             
+my_pen = turtle.Turtle()      
+for i in range(5):
+   my_pen.forward(250)           
+   my_pen.right(144)               
+turtle.done()
