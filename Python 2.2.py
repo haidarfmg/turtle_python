f8b84ee53f07fe7fdaaa196acac85a34b1e6cad1
Hayder K. Alhamadi <50412844+haidarfmg@users.noreply.github.com>
2022-04-20 11:57:48 +0300

    area and volume of cylinder

---
+++ Python 2.2.py
@@ -0,0 +1,7 @@
+radius=eval(input("Enter the radius  of a cylinder:"))
+length=eval(input("Enter the length  of a cylinder:"))
+pi=3.14
+area=(radius*radius*pi)
+volume=(area*length)
+print(("The area is",area))
+print(("The volume is",volume))
