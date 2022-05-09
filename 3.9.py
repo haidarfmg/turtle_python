11d15efff74bd8afda3ab40b7f06400178cbea6c
Hayder K. Alhamadi <50412844+haidarfmg@users.noreply.github.com>
2022-05-10 01:18:26 +0300

    Financial application payroll

---
+++ 3.9.py
@@ -0,0 +1,14 @@
+# Financial application payroll
+
+val1 = input("Enter employee's name: ")
+val2 = input("Enter number of hours worked in a week: ")
+val3 = input("Enter hourly pay rate: ")
+val4 = input("Enter federal tax withholding rate: ")
+val5 = input("Enter state taax withholding rate: ")
+
+
+print("employee name :", val1)
+print("number of hours worked in a week :", val2)
+print("hourly pay rate :", val3)
+print("federal tax withholding rate :", val4)
+print("state taax withholding rate :", val5)
